fe7f7f6d0103478d5645522f1e3b91088219068c
Kareem Campbell <kareemd.campbell@gmail.com>
2022-02-27 23:06:36 -0500

    Reset function and working on winner function

--- game.js
+++ game.js
@@ -1,23 +1,50 @@
 const gameBoard = (() =>{ 
-        const choiceA = document.querySelector("#x");
-        const choiceB = document.querySelector("#o");          
+        //const choice = document.querySelector("button");       
         const sectors = document.querySelectorAll(".sector");
         
-        let sector = Array.from(sectors);  
-        
-        choiceA.addEventListener('click',()=>{
-            sector.forEach((mark)=>{                
+        let sector = Array.from(sectors);
+        let choice = "X"; 
+        const gamePlay = () =>{
+            sector.forEach((mark)=>{     
                 mark.addEventListener('click',()=>{
-                    mark.textContent = "X";
-                })
+                    if(mark.textContent != "")
+                    return
+                    mark.textContent = choice;
+                    choice = choice === "X"?"O":"X";                                      
+                })                              
             })
-        })
-
-        choiceB.addEventListener('click',()=>{
-            sector.forEach((mark)=>{
-                mark.addEventListener('click',()=>{
-                    mark.textContent = "O";
+            const resetGame = () =>{
+                const reset = document.querySelector("#reset");
+                reset.addEventListener('click',()=>{
+                    sector.forEach((mark) =>{
+                        mark.textContent = "";
+                    })
                 })
-            })
-        })
+            }
+            resetGame();
+            const gameWinner = () =>{
+                const winner = [
+                    [0,1,2],
+                    [3,4,5],
+                    [6,7,8],
+                    [0,3,6],
+                    [1,4,7],
+                    [2,5,8],
+                    [0,4,8],
+                    [2,4,6]
+                ];
+                sector.forEach((mark)=>{
+                    let index = mark.textContent.indexOf("X");
+                    if(index === winner[index]){
+                        console.log("winner!!!");
+                    }
+                })
+            }
+            gameWinner();
+        }
+        const players = (player1, player2, machine)=>{
+            
+        }
+        return {gamePlay};       
     })();
+    gameBoard.gamePlay();
