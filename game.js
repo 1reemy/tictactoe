1fa42dd74b15ba5cd108aae1c0905f92757b48a2
Kareem Campbell <kareemd.campbell@gmail.com>
2022-04-12 21:27:25 -0500

    working on mini max function

--- game.js
+++ game.js
@@ -14,17 +14,19 @@ const gameBoard = (() =>{
         [2,4,6]
     ];
 
-    let choice;
     let play = Math.floor(Math.random() * sector.length);
-    //let validMove = [];               
+    let validMove = [];  
+    let finalFour = [];
+    let boardState = [];
+    let availMoves = [];
     const Players = (humanPlayer)=>{        
         const machine = () => { 
             while(sector[play].textContent !== ""){
                 play = Math.floor(Math.random() * sector.length);
             }
-            sector[play].textContent = "X";
-            /*validMove += play;
-            console.log(validMove.length)*/                       
+            sector[play].textContent = "X";           
+            validMove.push(sector[play].textContent);
+            //console.log(validMove)                       
         }
         return{humanPlayer, machine}
     }
@@ -32,12 +34,35 @@ const gameBoard = (() =>{
     const player = Players("O");
 
     const gamePlay = () =>{
+    const emptySquares = () =>{
+        availMoves = boardState.concat(validMove);
+        if(availMoves.length === 4){
+            finalFour.push(sector.filter((empty) => empty.textContent === ""));
+            console.log(finalFour);
+        }               
+    }        
+    /*const minmax = () => {
+            let availSpots = emptySquares();
+            for(let i = 0; i <= sector.length; i++){
+                let win = sector.push(availSpots);
+                if(winner[i] === win){
+                    player.machine();
+                    return {score:-10};
+                }else{
+                    player.humanPlayer;
+                    return {score:10};
+                }
+            }
+        }*/
         const legitComputerMove = () =>{            
             if(!boardFull()){
-                player.machine()
+                player.machine();                            
+                emptySquares();
+                //minmax();
             }
         }
         const boardFull = () => sector.every((val) => val.textContent != "");
+
         const gameOver = () => sector.forEach((spot) =>{
             if(spot != player.humanPlayer || spot != player.machine()){
                      spot.classList.add("gameover");
@@ -45,14 +70,17 @@ const gameBoard = (() =>{
          })
         const move = () =>{            
             sector.forEach((mark)=>{                
-                let step = ()=>{
-                    if(mark.textContent === ""){
-                    mark.textContent = player.humanPlayer; 
+                let step = ()=>{                    
+                    if(mark.textContent === ""){                    
+                    mark.textContent = player.humanPlayer;
+                    boardState.push(mark.textContent); 
                     legitComputerMove();
-                    gameWinner();                    
+                    gameWinner();
+                    //console.log(boardState);                    
                     }                                                                     
                 }
-                mark.addEventListener("click",step);
+                mark.addEventListener("click",step);  
+                                             
             })                
         }
         const resetGame = () =>{
@@ -72,6 +100,9 @@ const gameBoard = (() =>{
         const gameWinner = () =>{
             let o = winner.find((combo)=>combo.every((idx) => sector[idx].textContent === player.humanPlayer));
             let x = winner.find((combo)=>combo.every((idx) => sector[idx].textContent === "X"));
+            /*let availMoves = [];
+                availMoves = boardState.concat(validMove);
+                console.log(availMoves);*/
             if(o){                    
                 highLight(o);
                 gameOver();
@@ -83,7 +114,7 @@ const gameBoard = (() =>{
             }
         };
         move();
-        resetGame();                        
+        resetGame();
     }    
     return {gamePlay};       
 })();
